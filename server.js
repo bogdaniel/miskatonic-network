f406ca03d5804d9b6c71f1e9f7a19c0544fb6d95
lordjancso <lordjancso@gmail.com>
2015-10-05 19:49:47 +0200

    Code formatting

--- server.js
+++ server.js
@@ -5,22 +5,22 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/web'));
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+io.on('connection', function (socket) {
+    console.log('a user connected');
 
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
 
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3000, function () {
+    console.log('listening on *:3000');
 });
