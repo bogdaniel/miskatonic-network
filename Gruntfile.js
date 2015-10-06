f16e0510b80069e041c00029fe6584752c00e905
lordjancso <lordjancso@gmail.com>
2015-10-06 07:46:44 +0200

    Bower and Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            build: {
+                files: {
+                    'web/build/script.js': [
+                        'resources/js/script.js'
+                    ]
+                }
+            }
+        },
+        concat: {
+            build: {
+                files: {
+                    'web/build/<%= pkg.name %>.min.js': [
+                        'bower_components/jquery/dist/jquery.min.js',
+                        'bower_components/bootstrap/dist/js/bootstrap.min.js'
+                    ],
+                    'web/build/<%= pkg.name %>.min.css': [
+                        'bower_components/bootstrap/dist/css/bootstrap.min.css',
+                        'bower_components/bootstrap/dist/css/bootstrap-theme.min.css'
+                    ]
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.registerTask('default', ['uglify', 'concat']);
+
+};
