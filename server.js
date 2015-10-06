f5e84cc47e77b2459b8a8890eb340ced787a0ed6
lordjancso <lordjancso@gmail.com>
2015-10-06 19:48:09 +0200

    Nunjucks added

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var app = express();
+var nunjucks = require('nunjucks');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use(express.static(__dirname + '/web'));
+app.use(express.static('views'));
+nunjucks.configure('/', {
+    autoescape: true,
+    express: app
+});
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.render('index.html', {username: 'THIS IS NOT WORKING'});
+});
+
+app.get('/cards', function (req, res) {
+    res.render('index.html', {username: 'IT WORKS!'});
 });
 
 io.on('connection', function (socket) {
@@ -21,6 +30,6 @@ io.on('connection', function (socket) {
     });
 });
 
-http.listen(3000, function () {
+http.listen(6666, function () {
     console.log('listening on *:3000');
 });
