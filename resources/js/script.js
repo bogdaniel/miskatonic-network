d6216cd5beda8e0df5db4189d5144053259cac8a
lordjancso <lordjancso@gmail.com>
2015-10-19 19:58:21 +0200

    Card active and status handling

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,12 +2,12 @@ $(function () {
     "use strict";
 
     var fadeHighlight;
-    $(document).on('mouseenter', '.card, .card-exhausted, .card-story, .card-resource', function () {
+    $(document).on('mouseenter', '.card-active, .card-exhausted, .card-story, .card-resource', function () {
         clearTimeout(fadeHighlight);
         var src = $(this).find('img').attr('src');
         var img = $('<img>').addClass('img-responsive').attr('src', src);
         $('.highlight').html(img);
-    }).on('mouseout', '.card, .card-exhausted, .card-story, .card-resource', function () {
+    }).on('mouseout', '.card-active, .card-exhausted, .card-story, .card-resource', function () {
         fadeHighlight = setTimeout(function () {
             $('.highlight img').fadeOut(400, function () {
                 $('.highlight').empty();
