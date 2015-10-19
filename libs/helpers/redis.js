4c1e501b79bd9954644740a12ee4b4cb9f918f5c
lordjancso <lordjancso@gmail.com>
2015-10-19 19:24:09 +0200

    Strict mode enabled

--- libs/helpers/redis.js
+++ libs/helpers/redis.js
@@ -1,3 +1,5 @@
+"use strict";
+
 exports.dataToJSON = function (data) {
     var cards = [];
 
--- libs/math.js
+++ libs/math.js
@@ -1,3 +1,5 @@
+"use strict";
+
 exports.randomIntInclusive = function (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
