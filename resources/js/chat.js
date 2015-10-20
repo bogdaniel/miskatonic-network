9d4a5078c39f7b67f8ae485c6a51ed129e2614c3
lordjancso <lordjancso@gmail.com>
2015-10-20 07:28:03 +0200

    Public chat refactored

---
+++ resources/js/chat.js
@@ -0,0 +1,61 @@
+$(function () {
+    function joinRoom(username, room) {
+        socket.emit('join', {
+            username: username,
+            room: room
+        });
+    }
+
+    var parsedUrl = parseUri(window.location.href);
+    var room = 'general';
+
+    if (parsedUrl.query) {
+        room = parsedUrl.query;
+    }
+
+    if (parsedUrl.path == '/') {
+        joinRoom(username, room);
+    }
+
+    $('form.chat').submit(function (e) {
+        e.preventDefault();
+
+        socket.emit('chatMessage', $('#m').val());
+        $('#m').val('');
+    });
+
+    socket.on('userList', function (users) {
+        $('.chat-users').empty();
+        $.each(users, function (key, user) {
+            $('.chat-users').prepend($('<div>').text(user.username));
+        });
+    });
+
+    socket.on('archiveMessages', function (messages) {
+        var container = $('.chat-messages');
+        container.empty();
+        $.each(messages, function (key, message) {
+            message = $.parseJSON(message);
+            container.append($('<div>').text(message.username + ', ' + message.created_at + ', ' + message.message));
+        });
+        container.scrollTop(container[0].scrollHeight);
+    });
+
+    socket.on('chatMessage', function (message) {
+        var container = $('.chat-messages');
+        container.append($('<div>').text(message.username + ', ' + message.created_at + ', ' + message.message));
+        container.scrollTop(container[0].scrollHeight);
+    });
+
+    $('a.channel').on('click', function (e) {
+        e.preventDefault();
+
+        var room = $(this).attr('href').substring(1);
+        window.history.replaceState(null, null, '?' + room);
+
+        $('a.channel').removeClass('active');
+        $(this).addClass('active');
+
+        joinRoom(username, room);
+    });
+});
