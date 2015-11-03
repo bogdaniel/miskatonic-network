4c0ae28f19638fc248d8f27142ca6fcea14505e6
lordjancso <lordjancso@gmail.com>
2015-11-03 21:22:41 +0100

    Uncommit all cards at the end of the turn

--- src/socket.io/play.js
+++ src/socket.io/play.js
@@ -436,7 +436,7 @@ exports.endPhase = function (socket) {
             player.actions = ['playCard', 'endPhase'];
         } else if (game.phase == 'operations') {
             if (game.turn == 1 && game.activePlayer == game.firstPlayer) {
-                game = endTurn(player.id, game);
+                game = endTurn(socket, game);
             } else {
                 game.phase = 'story';
                 game.step = 'playerCommit';
@@ -453,7 +453,7 @@ exports.endPhase = function (socket) {
 
                 game.activePlayer = opponent.id;
             } else {
-                game = endTurn(player.id, game);
+                game = endTurn(socket, game);
             }
         } else if (game.phase == 'story' && game.step == 'opponentCommit') {
             game.phase = 'story';
@@ -471,7 +471,7 @@ exports.endPhase = function (socket) {
                 }
             }
 
-            game = endTurn(player.id, game);
+            game = endTurn(socket, game);
         }
 
         game = gameHelper.updatePlayer(game, player);
@@ -809,7 +809,7 @@ exports.endTurn = function (socket) {
             return false;
         }
 
-        game = endTurn(player.id, game);
+        game = endTurn(socket, game);
         Game.update(game);
 
         socket.emit('gameInfo', gameHelper.gameInfo(game, player.id));
@@ -817,9 +817,9 @@ exports.endTurn = function (socket) {
     });
 };
 
-function endTurn(playerId, game) {
-    var player = gameHelper.player(game, playerId);
-    var opponent = gameHelper.opponent(game, playerId);
+function endTurn(socket, game) {
+    var player = gameHelper.player(game, socket.userId);
+    var opponent = gameHelper.opponent(game, socket.userId);
 
     if (game.activePlayer != game.firstPlayer) {
         game.turn += 1;
@@ -843,6 +843,9 @@ function endTurn(playerId, game) {
     game.step = null;
     game = gameHelper.updatePlayer(game, player);
     game = gameHelper.updatePlayer(game, opponent);
+
+    socket.emit('turnEnded');
+    socket.broadcast.emit('turnEnded');
 
     return game;
 }
